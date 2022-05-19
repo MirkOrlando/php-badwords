a4e400fe42cc04c7775eec6980889c78e2799eea
MirkOrlando <mirko@orlando-m.it>
2022-05-19 15:24:42 +0200

    create php-badwords

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
 <?php
-echo 'Hello'
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP-badwords</title>
-</head>
-<body>
-    
-</body>
-</html>
+/* Creare una variabile con 
+un paragrafo di testo a vostra scelta. */
+ $text = '
+ Io sono un disonesto, 
+ e un disonesto puoi sempre confidare che sia disonesto. 
+ Onestamente parlando, 
+ è dagli onesti che devi guardarti, 
+ perché non puoi mai prevedere quando faranno qualcosa di incredibilmente stupido.
+ ';
+
+ /* Stampare a schermo il paragrafo e la sua lunghezza. */
+ var_dump($text);
+
+ /* Una parola da censurare viene 
+passata dall'utente tramite parametro GET. */
+$bad_word = $_GET['bad_word'];
+var_dump($bad_word);
+
+/* Stampare di nuovo il paragrafo e la sua lunghezza, 
+dopo aver sostituito con tre asterischi (***) tutte 
+le occorrenze della parola da censurare. */
+$new_text = str_replace($bad_word,'***',$text);
+var_dump($new_text);
+
+?>
